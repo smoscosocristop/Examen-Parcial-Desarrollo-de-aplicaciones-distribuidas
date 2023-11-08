456848d754a8c96fe7f0b4fb22eebd810910de29
smoscosocristop <smoscosocristop>
2023-11-07 20:32:23 -0500

    update3

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -6,7 +6,7 @@ function SideBar() {
     <div>
       <div className="fixed top-0 left-0 z-50 w-64 h-full p-4 bg-gray-900">
         <a href="#" className="flex items-center pb-4 border-b border-b-gray-600">
-          <span className="ml-3 text-lg font-bold text-white">DAD USS</span>
+          <span className="ml-3 text-lg font-bold text-white">Examen parcial</span>
         </a>
         <ul className="mt-4">
           <li className="mb-1 group active">
@@ -17,7 +17,7 @@ function SideBar() {
             </a>
           </li>
           <li className="mb-1 group active">
-            <a href="#" className="flex items-center px-4 py-2 text-gray-300 bg-gray-800 rounded-md hover:bg-gray-950 hover:text-gray-100">
+            <a href="/propiedades" className="flex items-center px-4 py-2 text-gray-300 bg-gray-800 rounded-md hover:bg-gray-950 hover:text-gray-100">
               <span className="text-sm">Propiedades</span>
             </a>
           </li>
